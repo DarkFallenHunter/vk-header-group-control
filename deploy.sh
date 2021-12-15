f9e03af3446655b7fdd03dccc7b3daeae3fbc939
DarkFallenHunter <yvoron97@yandex.ru>
2021-12-16 01:13:38 +0300

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ pip3 install -r requirements.txt
 mkdir logs
 
 crontab -l > mycron
-echo "*       */1       *       *       * python3 $(pwd)/run.sh" >> mycron
+echo "*       */1     *       *       *       /root/vk-header-group-control/run.sh" >> mycron
 crontab mycron
 rm mycron
